65f5efe1f3848b45b8acb3c29c07271b04428ede
sun <xiaoxing440824@outlook.com>
2019-12-09 17:19:20 +0800

    update for pusher socket

---
+++ app/Events/NoteEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NoteEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $user;
+    protected $channel;
+    protected $event;
+    public function __construct($user, $event = 'note.created')
+    {
+        $this->user = $user;
+        $this->channel = ["note"];
+        $this->event = $event;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return $this->channel;
+    }
+    public function broadcastAs()
+    {
+        return $this->event;
+    }
+}
